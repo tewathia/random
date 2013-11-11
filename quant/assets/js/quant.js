f4a8ec3c19ccc57bf89c6b4cca12d2a5b02caf9a
Anirudh Tewathia <atewathia@sapient.com>
2013-11-11 14:50:10 +0530

    CnB

--- quant/assets/js/quant.js
+++ quant/assets/js/quant.js
@@ -1,8 +1,8 @@
 var quant = {
-	factors : function (n){
+	factors: function(n) {
 		var factors = [];
-		for (var i = 0; i <= n/2; i++){
-			if (n%i===0){
+		for (var i = 0; i <= n / 2; i++) {
+			if (n % i === 0) {
 				factors.push(i);
 			}
 		}
@@ -11,19 +11,19 @@ var quant = {
 	},
 
 
-	tripletsCount : function (n) {
+	tripletsCount: function(n) {
 		var count = 0;
 		var factorsList = this.factors(n);
 		oneCount = 0;
-		var f = 2310/2;
-		for (var i = 1; i<=f; i++){
-			for (var j = i; j<=f; j++){
-				for (var k = j; k<=f; k++){
-					if (i*j*k === n){
+		var f = 2310 / 2;
+		for (var i = 1; i <= f; i++) {
+			for (var j = i; j <= f; j++) {
+				for (var k = j; k <= f; k++) {
+					if (i * j * k === n) {
 						count += 1;
-						console.log(i, j, k, i*j*k);
-						if (i==1) {
-							oneCount+=1;
+						console.log(i, j, k, i * j * k);
+						if (i == 1) {
+							oneCount += 1;
 						}
 					}
 				}
@@ -35,23 +35,21 @@ var quant = {
 
 	Lottery: function() {
 		var _array = [],
-		args = arguments,
-		isArray = (args[0] instanceof Array);
-		if(!isArray) {
+			args = arguments,
+			isArray = (args[0] instanceof Array);
+		if (!isArray) {
 			for (var i in args) {
 				_array.push(args[i]);
 			}
-		}
-		else {
+		} else {
 			_array = args[0];
 		}
 		this.pickWinner = function() {
 			if (_array.length > 0) {
-				var _winnerIndex = Math.floor(Math.random()*(_array.length));
+				var _winnerIndex = Math.floor(Math.random() * (_array.length));
 				var _winner = _array.splice(_winnerIndex, 1);
 				return _winner;
-			}
-			else {
+			} else {
 				throw new Error('No More Winners can be picked!');
 			}
 		};
@@ -60,8 +58,8 @@ var quant = {
 		};
 	},
 
-	CowsAndBulls: function (num) {
-		var n = ((num<10) && (num>0)) ? num : 4;
+	CowsAndBulls: function(num) {
+		var n = ((num < 10) && (num > 0)) ? num : 4;
 		var _generateRandomNumber = function(n) {
 			var _numArr = [];
 			var _finalNum = 0;
@@ -69,32 +67,32 @@ var quant = {
 				_numArr.push(i);
 			}
 			for (var j = 0; j < n; j++) {
-				var _index = Math.floor(Math.random()*_numArr.length);
-				_finalNum += _numArr[_index]*Math.pow(10, j);
+				var _index = Math.floor(Math.random() * _numArr.length);
+				_finalNum += _numArr[_index] * Math.pow(10, j);
 				_numArr.splice(_index, 1);
 			}
 			return _finalNum;
 		};
 		var _toBeGuessed = _generateRandomNumber(n);
 
-		this.giveUp = function () {
+		this.giveUp = function() {
 			console.log('The Number was ' + _toBeGuessed);
 			return _toBeGuessed;
 		};
 
 		this.log = [],
 
-		this.isIt = function (guess) {
-			var _getCount = function (string) {
+		this.isIt = function(guess) {
+			var _getCount = function(string) {
 				var _countObj = {};
 				var _guessString = guess + '';
 				var _toBeGuessedString = _toBeGuessed + '';
 				var _cows = 0;
 				var _bulls = 0;
-				var _checkGuessFormat = function (val) {
+				var _checkGuessFormat = function(val) {
 					var _isProper = false;
 					var _indexSum = 0;
-					var _expectedIndexSum = (9-val)*(-1) + val*(val-1)/2;
+					var _expectedIndexSum = (9 - val) * (-1) + val * (val - 1) / 2;
 					for (var i = 1; i < 10; i++) {
 						_indexSum += _guessString.indexOf(i);
 					}
@@ -119,7 +117,7 @@ var quant = {
 						}
 					}
 				}
-				_countObj['Cows'] = _cows;
+				_countObj['Cows'] = _cows-_bulls;
 				_countObj['Bulls'] = _bulls;
 				return _countObj;
 			};
@@ -135,7 +133,7 @@ var quant = {
 				console.log('Bingo! You have guessed ' + guess + ' correctly.');
 				return;
 			}
-			console.log('Your guess ' + guess + ' contains ' + _cowCount + ' Cow' + (_cowCount===1?'':'s') + ' and ' + _bullCount + ' Bull' + (_bullCount===1?'':'s') + '.');
+			console.log('Your guess ' + guess + ' contains ' + _cowCount + ' Cow' + (_cowCount === 1 ? '' : 's') + ' and ' + _bullCount + ' Bull' + (_bullCount === 1 ? '' : 's') + '.');
 			return _finalCountObj;
 		}
 	},
@@ -151,7 +149,7 @@ var quant = {
 				var cell = rowArray[cellInput]
 				var cellObj = {
 					value: cell,
-					state: (cell===0?'Blank':'Given')
+					state: (cell === 0 ? 'Blank' : 'Given')
 				};
 				if (cellObj.state == 'Given') {
 					this.complexity--;
@@ -183,11 +181,11 @@ var quant = {
 				}
 			};
 			var _checkBox = function(box) {
-				var x =	(box%3)*3,
-				y = (Math.floor(box/3))*3;
+				var x = (box % 3) * 3,
+					y = (Math.floor(box / 3)) * 3;
 				_sum = 0;
-				for (var i = x; i < x+3; i++) {
-					for(var j = y; j < y+3; j++) {
+				for (var i = x; i < x + 3; i++) {
+					for (var j = y; j < y + 3; j++) {
 						_sum += sudoku[i][j].value;
 					}
 				}
@@ -205,22 +203,22 @@ var quant = {
 			return _isCorrect;
 		}
 
-		this.id = Math.floor(Math.random()*1000);
+		this.id = Math.floor(Math.random() * 1000);
 
-		this.show =  function()  {
-			if($('.sudokuTable'+this.id).length > 0) {
+		this.show = function() {
+			if ($('.sudokuTable' + this.id).length > 0) {
 				this.remove();
 			}
 			var formattedSudoku = '';
 			for (var row in problem) {
-				formattedSudoku+='<tr>';
+				formattedSudoku += '<tr>';
 				for (var cell in problem[row]) {
 					var cellObj = problem[row][cell];
-					formattedSudoku+= '<td class="' + cellObj.state + '">' + cellObj.value + '</td>';
+					formattedSudoku += '<td class="' + cellObj.state + '">' + cellObj.value + '</td>';
 				}
-				formattedSudoku+='</tr>';
+				formattedSudoku += '</tr>';
 			}
-			var table = $('<table>').attr('class', 'sudokuTable'+this.id).html(formattedSudoku);
+			var table = $('<table>').attr('class', 'sudokuTable' + this.id).html(formattedSudoku);
 			table.appendTo($('body'));
 			return problem;
 		}
@@ -232,7 +230,7 @@ var quant = {
 		this.iterations = 0;
 		this.stackCount = 0;
 
-		this.solve =  function()  {
+		this.solve = function() {
 			this.iterations++;
 			var attempt = problem;
 			if (!this.fillerArray.length) {
@@ -260,7 +258,7 @@ var quant = {
 					var cell = row[i];
 					if (cell.state !== 'Given') {
 						cell.state = 'Solved';
-						var _index = Math.floor(Math.random()*_remArr.length);
+						var _index = Math.floor(Math.random() * _remArr.length);
 						cell.value = _remArr[_index];
 						_remArr.splice(_index, 1);
 					}
@@ -269,49 +267,47 @@ var quant = {
 			for (var i in attempt) {
 				// console.log(i, 'attempt');
 				_fillRow(attempt[i], i);
-			// console.log(attempt[i]);
-		}
-		var isSolved = this.checkSolution(attempt);
-		var maxStackSize = 5000;
-		// console.log(isSolved); 
-		if (isSolved) {
-			var solution = attempt;
-			var totalIterations = this.iterations+this.stackCount*maxStackSize;
-			this.show();
-			console.log('!Solved!', 'stack', this.stackCount, 'iterations', totalIterations, solution);
-			this.iterations = 0;
-			this.stackCount = 0;
-			var lSIC = JSON.parse(localStorage.getItem('SudokuIterations')) || {};
-			lSIC['C' + this.complexity] = lSIC['C' + this.complexity] || [];
-			lSIC['C' + this.complexity].push(totalIterations);
-			localStorage.setItem('SudokuIterations', JSON.stringify(lSIC));
-			var that = this;
-			if (lSIC['C' + this.complexity].length < 100) {
-				console.log(lSIC['C' + this.complexity].length);
-				setTimeout(function () {
-					that.solve();
-				}, 1)
+				// console.log(attempt[i]);
 			}
-		}
-		else {
-			// console.log('!Not Solved');
-			if (this.iterations === maxStackSize) {
+			var isSolved = this.checkSolution(attempt);
+			var maxStackSize = 5000;
+			// console.log(isSolved); 
+			if (isSolved) {
+				var solution = attempt;
+				var totalIterations = this.iterations + this.stackCount * maxStackSize;
+				this.show();
+				console.log('!Solved!', 'stack', this.stackCount, 'iterations', totalIterations, solution);
+				this.iterations = 0;
+				this.stackCount = 0;
+				var lSIC = JSON.parse(localStorage.getItem('SudokuIterations')) || {};
+				lSIC['C' + this.complexity] = lSIC['C' + this.complexity] || [];
+				lSIC['C' + this.complexity].push(totalIterations);
+				localStorage.setItem('SudokuIterations', JSON.stringify(lSIC));
 				var that = this;
-				setTimeout(function() {
-					that.show();
-					that.iterations = 0;
-					that.stackCount++;
-					that.solve();
-				}, 1);
-			}
-			else {
-				this.solve();
+				if (lSIC['C' + this.complexity].length < 100) {
+					console.log(lSIC['C' + this.complexity].length);
+					setTimeout(function() {
+						that.solve();
+					}, 1)
+				}
+			} else {
+				// console.log('!Not Solved');
+				if (this.iterations === maxStackSize) {
+					var that = this;
+					setTimeout(function() {
+						that.show();
+						that.iterations = 0;
+						that.stackCount++;
+						that.solve();
+					}, 1);
+				} else {
+					this.solve();
+				}
 			}
 		}
+		this.getStats = function() {
+			return JSON.parse(localStorage.getItem('SudokuIterations'));
+		}
+		this.show();
 	}
-	this.getStats = function() {
-		return JSON.parse(localStorage.getItem('SudokuIterations'));
-	}
-	this.show();
-}
 };
